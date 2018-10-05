2e3a5c8444d6522bf9a62cecda2a58ce46595e8d
Samuel Cole <cole.samuel21@gmail.com>
2018-10-05 18:05:16 -0500

    Add boilerplate files

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <p>This is an example component</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
